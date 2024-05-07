792232c6ac4d9617673bff003e5b7a28a538e1a2
BuildTools <unconfigured@null.spigotmc.org>
2024-05-07 13:56:07 -0400

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
 import './App.css'
+import { puppyList } from './data.js'
+import { useState } from 'react'
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  console.log("puppyList: ", puppyList)
+  
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
   return (
     <>
-      
+    <div className='App'>
+      <div>
+      {
+        
+        puppies.map((puppy) => {
+            
+
+             return (
+              <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+              {puppy.name}
+              </p>
+              
+             );
+            })
+       }
+       </div>
+       
+       {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+       
+    </div>
     </>
   )
 }
